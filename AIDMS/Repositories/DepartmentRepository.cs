d2bb861977bcf49dc04ff573ea0030151f30837f
YoussefGemy <youssef.gemy00@gmail.com>
2024-05-17 21:35:12 +0300

    end of Repository

---
+++ AIDMS/Repositories/DepartmentRepository.cs
@@ -0,0 +1,49 @@
+﻿using AIDMS.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AIDMS.Repositories;
+
+public class DepartmentRepository:IDepartmentRepository
+{
+    private readonly AIDMSContextClass _context;
+
+    public DepartmentRepository(AIDMSContextClass context) {
+        _context = context;
+    }
+    
+    public async Task<Department> GetDepartmentByIdAsync(int DepartmentId)
+    {
+        return await _context.Departments.FirstOrDefaultAsync(i => i.Id == DepartmentId);
+    }
+
+    public async Task<List<Department>> GetAllDepartmentsAsync()
+    {
+        return await _context.Departments.ToListAsync();
+    }
+
+    public async Task AddDepartmentAsync(Department Department)
+    {
+        _context.Departments.Add(Department);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateDepartmentAsync(int DepartmentId, Department Department)
+    {
+        var existingDepartment= await GetDepartmentByIdAsync(DepartmentId);
+        if (existingDepartment == null) {
+            throw new InvalidOperationException($"Department with ID {DepartmentId} not found.");
+        }
+        _context.Entry(existingDepartment).CurrentValues.SetValues(Department);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteDepartmentAsync(int DepartmentId)
+    {
+        var departmentToDelete= await GetDepartmentByIdAsync(DepartmentId);
+        if (departmentToDelete == null) {
+            throw new InvalidOperationException($"Department with ID {DepartmentId} not found.");
+        }
+        _context.Departments.Remove(departmentToDelete);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ AIDMS/Repositories/ISupervisorRepository.cs
@@ -0,0 +1,11 @@
+﻿using AIDMS.Entities;
+
+namespace AIDMS.Repositories;
+public interface ISupervisorRepository
+{
+    Task<Supervisor> GetSupervisorByIdAsync(int SupervisorId);
+    Task<List<Supervisor>> GetAllSupervisorAsync();
+    Task AddSupervisorAsync(Supervisor Supervisor);
+    Task UpdateSupervisorAsync(int SupervisorId, Supervisor Supervisor);
+    Task DeleteSupervisorAsync(int SupervisorId);
+}
