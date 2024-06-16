d9307bca35fbbccc4d688ea8572963cc425b37d5
Moustafa Gamal <Moustafa.Gamal672@gmail.com>
2024-06-17 02:27:55 +0300

    Done Let's Merge

--- AIDMS/obj/Debug/net8.0/AIDMS.AssemblyInfo.cs
+++ AIDMS/obj/Debug/net8.0/AIDMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54574451396f3451ca2d35fe7ba8102cd25343a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b766c74c966fb23e9d815cede3d2fc9dc6af2fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
