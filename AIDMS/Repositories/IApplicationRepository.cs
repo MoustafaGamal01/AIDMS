f6ba93aac7133e07d194c621fadd510121abdaa5
Youssef Gamal <75047282+YoussefGemy@users.noreply.github.com>
2024-06-17 03:06:10 +0300

    trying to fix IApplicationRepository

--- AIDMS/Repositories/IApplicationRepository.cs
+++ AIDMS/Repositories/IApplicationRepository.cs
@@ -1,4 +1,4 @@
-﻿using AIDMS.Entities;
+using AIDMS.Entities;
 
 namespace AIDMS.Repositories
 {
@@ -7,20 +7,13 @@ namespace AIDMS.Repositories
         Task<Application> GetApplicationByIdAsync(int applicationId);
         Task<List<Application>> GetAllApplicationsAsync();
         Task<bool?> UpdateApplicationAsync(int applicationId, Application application);
-        Task DeleteApplicationAsync(int applicationId);
+        Task<bool?> DeleteApplicationAsync(int applicationId);
         Task<List<Application>> GetAllApplicationsByStudentIdAsync(int studentId);
         Task<List<Application>> GetAllApplicationsByStudentNameAsync(string studentName);
         Task<List<Application>> GetAllPendingApplicationsAsync();
         Task<List<Application>> GetAllPendingApplicationsByStudentIdAsync(int studentId);
         Task<List<Application>> GetAllPendingApplicationsByEmployeeIdAsync(int empId);
         
-<<<<<<< HEAD
-        // Task<List<Application>> GetAllArchivedApplicationsAsync();
-        // Task<List<Application>> GetAllArchivedApplicationsByStudentIdAsync();
-        //Task<List<Application>> GetAllReviewedApplicationsWithStudentRelatedAsync(int empId);
-        Task<List<Application>> GetAllArchivedApplicationsWithStudentRelatedAsync();
-=======
->>>>>>> main
         Task<List<Application>> GetAllArchivedApplicationsByEmployeeIdAsync(int empId);
         
         Task<List<Application>> GetAllArchivedApplicationsAsync();
