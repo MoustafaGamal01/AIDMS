333219a1e294a1d6c6c659f151645960f5feed1a
Moustafa Gamal <Moustafa.Gamal672@gmail.com>
2024-06-18 14:31:19 +0300

    Mod EmpController

--- AIDMS/obj/Debug/net8.0/AIDMS.AssemblyInfo.cs
+++ AIDMS/obj/Debug/net8.0/AIDMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd1fa5513f553ca2e16c140abbc766517fe9e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d5c6c86fcd07c631625a94f9242cda021a8fbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
