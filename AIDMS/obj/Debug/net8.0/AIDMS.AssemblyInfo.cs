6d227b10e46585f698065359a6923b5d164afe77
Moustafa Gamal <Moustafa.Gamal672@gmail.com>
2024-06-18 14:35:34 +0300

    Finish ressolving

--- AIDMS/obj/Debug/net8.0/AIDMS.AssemblyInfo.cs
+++ AIDMS/obj/Debug/net8.0/AIDMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cca73393c45f94430f84546f16f5832bfc0ce0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09262623dbeddd83ebb6c07e9f5de101c1adc7ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIDMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
